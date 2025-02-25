cad6ce0d4b585544751327de56a088a7e9cfef87
Karuitha <diakingathia2005@gmail.com>
2025-02-25 15:36:33 +0200

    Added projejct for the first time

---
+++ maths.py
@@ -0,0 +1,98 @@
+# Arithmetic operations
+print(5 + 6)
+print(7 - 3)
+print(9 * 2)
+print(10 / 2)
+
+# Exponentiation
+print(2 ** 3)
+
+# Modulus
+print(10 % 3)
+
+# Floor division
+print(10 // 3)
+
+# String concatenation
+print("Hello" + " " + "World!")
+
+
+# Atomic data structures in Python
+# 1. float
+# Can have fractions
+type(3.14)
+
+# 2. int
+type(2)
+
+
+# 3. str
+type("hello")
+print("I'm an adult!")
+
+# 4. bool
+type(True)
+type(False)
+
+# The concept of a variable and variable assignment
+# Assignment operator (=)
+my_year_of_birth = 1990
+print(my_year_of_birth)
+
+2025 - my_year_of_birth
+
+# Equality (==)
+50 == 50
+
+# Height (cm)
+height_in_cm = 180
+height_in_meters = height_in_cm / 100
+
+# Weight (kg)
+weight_in_kg = 70
+
+# BMI calculation
+bmi = weight_in_kg / (height_in_meters ** 2)
+print(bmi)
+
+# My Names 
+my_name = "John Karuitha"
+print(my_name)
+
+my_lucky_number = 5
+
+my_float = 3.14
+
+admitted = False
+
+# Comparison operations
+# == (equality)
+# >=
+# <= 
+# >
+# <
+
+# Logical operations
+# and
+# or
+# not
+
+True or False
+True and False
+False and False
+not True and not False
+
+# Functions and Methods in Python
+
+
+# Complex data types 
+# Lists
+
+
+# Tuples
+
+
+# Sets
+
+
+# Dictionaries
